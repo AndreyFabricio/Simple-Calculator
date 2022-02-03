b06401a55fcb23ee9c6bee6de8e6eeddf0c7b752
Andrey Fabricio <andrey.fas@gmail.com>
2022-02-03 19:32:59 -0300

    Code documentation
    Proper documentation of the code

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -19,12 +19,14 @@ import java.text.DecimalFormat;
 
 public class SimpleCalculator extends JFrame implements ActionListener {
 
-	private DecimalFormat df = new DecimalFormat("#.0000");
+	// private DecimalFormat df = new DecimalFormat("#.0000");
 	
+	// Symbols of the first line of buttons
 	private String[] cSymb = {
 			"CE", "C", "DEL"
 	};
 	
+	// Rest of symbols
 	private String[] symb = {
 			"√", "x²", "%", "X",
 			"7", "8", "9", "÷",
@@ -33,70 +35,79 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 			"±", "0", ".", "="
 	};
 	
-	private JPanel panel = new JPanel(new BorderLayout(2, 2));
-	private JPanel pan1 = new JPanel(new BorderLayout(1, 1));
-	private JPanel pan2 = new JPanel(new BorderLayout(1, 1));
-	private JPanel cPan = new JPanel(new GridLayout(1, 3, 1, 1));
-	private JPanel btnPan = new JPanel(new GridLayout(5, 4, 1, 1));
-	private JButton[] cBtns = new JButton[3];
-	private JButton[] btns = new JButton[20];
+	private JPanel panel = new JPanel(new BorderLayout(2, 2)); // Outer panel
+	private JPanel pan1 = new JPanel(new BorderLayout(1, 1)); // Text panel
+	private JPanel pan2 = new JPanel(new BorderLayout(1, 1)); // Buttons panel
+	private JPanel cPan = new JPanel(new GridLayout(1, 3, 1, 1)); // Button panel group 1
+	private JPanel btnPan = new JPanel(new GridLayout(5, 4, 1, 1)); // Button panel group 2
+	private JButton[] cBtns = new JButton[3]; // Buttons of "cSymb"
+	private JButton[] btns = new JButton[20]; // Buttons of "symb"
 	
-	JLabel calcText = new JLabel("0");
-	JLabel holdText = new JLabel(" ");
+	JLabel calcText = new JLabel("0"); // Lower text (Typed text and/or results)
+	JLabel holdText = new JLabel(" "); // Upper text (equation)
 	
-	private int opt = 0;
+	private int opt = 0; // Holds the equation
 	
-	private double firstN = 0, secN = 0, holdN = 0;
+	private double firstN = 0, secN = 0, holdN = 0; // Holds the numbers for the equations
 	
-	private boolean repeat = false;
+	private boolean repeat = false; // Checks if the equation should be repeated
 	
 	public SimpleCalculator()
-	{
+	{ 
 		addComponentsToPanels();
 	}
 	
 	private void addComponentsToPanels() {
 		
+		// Adds the first roll of buttons (C, CE, DEL)
 		for(int i = 0; i < cSymb.length; i++) {
 			
-			cBtns[i] = new JButton(cSymb[i]);
+			cBtns[i] = new JButton(cSymb[i]); // Creates the button
 			
+			// Configures the appearance of the button
 			cBtns[i].setOpaque(false);
 			cBtns[i].setBorderPainted(false);
 			cBtns[i].setFont(new Font("Arial", Font.BOLD, 22));
 			cBtns[i].setFocusPainted(false);
-			cBtns[i].addActionListener(this);
-			cPan.add(cBtns[i]);
+			
+			cBtns[i].addActionListener(this); // Add an action listener to the button
+			cPan.add(cBtns[i]); // Add button to panel
 		}
+		// Adds the remaining buttons
 		for(int i = 0; i < symb.length; i++) {
 			
-			btns[i] = new JButton(symb[i]);
+			btns[i] = new JButton(symb[i]); // Creates the button
 			
+			// Configures the appearance of the button
 			btns[i].setBorderPainted(false);
 			btns[i].setOpaque(false);
 			btns[i].setFont(new Font("Arial", Font.BOLD, 22));
 			btns[i].setFocusPainted(false);
-			btns[i].addActionListener(this);
-			btnPan.add(btns[i]);
+			
+			btns[i].addActionListener(this); // Add an action listener to the button
+			btnPan.add(btns[i]); // Add button to panel
 		}
 
+		// Function maps the buttons after creation
 		keyboardMap();
 		
+		// Create an border around the text
 		pan1.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1));
 		
+		// Configures the appearance of the text
 		calcText.setFont(new Font("Serif",Font.BOLD,30)); // Sets the font size, location, etc
 		calcText.setHorizontalAlignment(SwingConstants.RIGHT); // Sets the text to be on the right
 		holdText.setFont(new Font("Serif",Font.PLAIN,20)); // Sets the font size, location, etc
 		holdText.setHorizontalAlignment(SwingConstants.RIGHT); // Sets the text to be on the right	
 		
-		pan1.add(holdText, BorderLayout.NORTH);
-		pan1.add(calcText, BorderLayout.CENTER);
-		pan2.add(cPan, BorderLayout.NORTH);
-		pan2.add(btnPan, BorderLayout.CENTER);
+		pan1.add(holdText, BorderLayout.NORTH); // Adds the upper text to the panel
+		pan1.add(calcText, BorderLayout.CENTER); // Adds the lower text to the panel
+		pan2.add(cPan, BorderLayout.NORTH); // Adds the first row of buttons to the panel
+		pan2.add(btnPan, BorderLayout.CENTER); // Adds the other buttons to the panel
 		
-		panel.add(pan1, BorderLayout.NORTH);
-		panel.add(pan2, BorderLayout.CENTER);
-		add(panel, BorderLayout.CENTER);
+		panel.add(pan1, BorderLayout.NORTH); // Adds panel with the text to the main panel
+		panel.add(pan2, BorderLayout.CENTER); // Adds panel with the buttons to the main panel
+		add(panel, BorderLayout.CENTER); // Adds panel to the application
 		
         setTitle("Simple Calculator"); // Sets the title
         setSize(357,417); // Defines the bounds of the frame
@@ -108,12 +119,12 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 	
 	private void keyboardMap() {
 		
-		// C, CE, DEL
+		// Maps C, CE and DEL buttons
 		clickOnKey( cBtns[0], "actionPerformed", KeyEvent.VK_SPACE );	
 		clickOnKey( cBtns[1], "actionPerformed", KeyEvent.VK_DELETE );	
 		clickOnKey( cBtns[2], "actionPerformed", KeyEvent.VK_BACK_SPACE );	
 		
-		// Operations (keyboard)
+		// Maps operations (keyboard)
 		clickOnKey( btns[3], "actionPerformed", KeyEvent.VK_ASTERISK );		
 		clickOnKey( btns[7], "actionPerformed", KeyEvent.VK_SLASH );
 		clickOnKey( btns[11], "actionPerformed", KeyEvent.VK_PLUS );		
@@ -121,7 +132,7 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 		clickOnKey( btns[18], "actionPerformed", KeyEvent.VK_PERIOD );
 		clickOnKey( btns[19], "actionPerformed", KeyEvent.VK_EQUALS );
 		
-		// Operations (numpad)
+		// Maps operations (numpad)
 		clickOnKey( btns[3], "actionPerformed", KeyEvent.VK_MULTIPLY );		
 		clickOnKey( btns[7], "actionPerformed", KeyEvent.VK_DIVIDE );
 		clickOnKey( btns[11], "actionPerformed", KeyEvent.VK_ADD );		
@@ -129,7 +140,7 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 		clickOnKey( btns[18], "actionPerformed", KeyEvent.VK_DECIMAL );
 		clickOnKey( btns[19], "actionPerformed", KeyEvent.VK_ENTER );		
 
-		// Numbers (keyboard)
+		// Maps numbers (keyboard)
 		clickOnKey( btns[17], "actionPerformed", KeyEvent.VK_0 );
 		clickOnKey( btns[12], "actionPerformed", KeyEvent.VK_1 );
 		clickOnKey( btns[13], "actionPerformed", KeyEvent.VK_2 );
@@ -141,7 +152,7 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 		clickOnKey( btns[5], "actionPerformed", KeyEvent.VK_8 );
 		clickOnKey( btns[6], "actionPerformed", KeyEvent.VK_9 );
 		
-		// Numbers (numpad)
+		// Maps numbers (numpad)
 		clickOnKey( btns[17], "actionPerformed", KeyEvent.VK_NUMPAD0 );
 		clickOnKey( btns[12], "actionPerformed", KeyEvent.VK_NUMPAD1 );
 		clickOnKey( btns[13], "actionPerformed", KeyEvent.VK_NUMPAD2 );
@@ -155,6 +166,7 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 		
 	}
 
+	// Calls button functionality when respective key is pressed
 	public static void clickOnKey(
 		    final AbstractButton button, String actionName, int key )
 		{
@@ -174,6 +186,7 @@ public class SimpleCalculator extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) 
     {
+    	// Gets the button symbol and does the respective action with "doActions"
     	String btnClick = e.getActionCommand();     	
     	doActions(btnClick);
     	
@@ -183,7 +196,7 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 		
 		switch(btnClick) {
     	
-		// Numbers
+		// Add numbers to "calcText"
 		case "0":
 			if(calcText.getText() != "0")
 				calcText.setText(calcText.getText() + "0");
@@ -253,8 +266,9 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 			repeat = false;
 			break;
 		
-		// Operations
+		// Does the operations
 		case "X":
+			// Multiplication
 			opt = 1;
 			firstN = Double.parseDouble(calcText.getText());
 			holdText.setText(numberFormating(firstN) + " X ");
@@ -262,6 +276,7 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 			repeat = false;
 			break;
 		case "÷":
+			// Division
 			opt = 2;
 			firstN = Double.parseDouble(calcText.getText());
 			holdText.setText(numberFormating(firstN) + "/");
@@ -269,6 +284,7 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 			repeat = false;
 			break;
 		case "+":
+			// Sum
 			opt = 3;
 			firstN = Double.parseDouble(calcText.getText());
 			holdText.setText(numberFormating(firstN) + " + ");
@@ -276,6 +292,7 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 			repeat = false;
 			break;
 		case "-":
+			// Subtraction
 			opt = 4;
 			firstN = Double.parseDouble(calcText.getText());
 			holdText.setText(numberFormating(firstN) + " - ");
@@ -283,16 +300,19 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 			repeat = false;
 			break;
 		case "√":
+			// Square root
 			firstN = Double.parseDouble(calcText.getText());
 			holdText.setText("√(" + numberFormating(firstN) + ")");
 			calcText.setText(""+ numberFormating(Math.sqrt(firstN)));
 			break;
 		case "x²":
+			// Squares
 			firstN = Double.parseDouble(calcText.getText());
 			holdText.setText("(" + numberFormating(firstN) + ")²");
 			calcText.setText(""+ numberFormating(Math.pow(firstN, 2)));
 			break;
 		case "%":
+			// Percentages
 			if (holdText.getText() != " ") {
 				secN = Double.parseDouble(calcText.getText());
 				runPercentage();
@@ -300,34 +320,40 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 			}
 			break;
 		case "=":
+			// Gives the result
 			if (holdText.getText() != " ") {
 				if (!repeat)
 					secN = Double.parseDouble(calcText.getText());
 				runopt();
-				repeat = true;
+				repeat = true; // With true, repeats the operation
 			}
 			break;
 			
 		// Formating
 		case "C":
+			// Deletes all text (resets the calculator)
 			calcText.setText("0");
 			holdText.setText(" ");
 			break; 
 		case "CE":
+			// Deletes current text (calcText)
 			calcText.setText("0");
 			break; 
 		case "DEL":
+			// Deletes last char of the current text (calcText)
 			if(calcText.getText().length() - 1 > 0)
 				calcText.setText(calcText.getText().substring(0, calcText.getText().length() - 1));
 			else
 				calcText.setText("0");
 			break; 
 		case "±":
+			// Changes signal of the number in "calcText"
 			firstN = Double.parseDouble(calcText.getText()) * (-1);
 			if (firstN != -0)
 				calcText.setText(numberFormating(firstN));
 			break;
 		case ".":
+			// Adds decimal places in the number on "calcText"
 			if(!calcText.getText().contains("."))
 				calcText.setText(calcText.getText() + ".");
 			break;    
@@ -342,29 +368,34 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 	private void runPercentage() {
 		
 		if(repeat) {
+			// Dont loses the percent value when user repeats the operation
 			secN = holdN;
 			firstN = Double.parseDouble(calcText.getText());
 		}
 		else {
+			// Holds the percentage
 			holdN = secN;
 		}
 		
+		// Gets the X% value and then runs the operations
 		secN = secN * firstN;
 		secN = secN / 100;
 		runopt();		
 
+		// Formating variables
 		String getSymbol = "";
 		double tempNum = 0;
 		
 		switch(opt) {
+		// Gets the current working symbol/operation
 		case 1:
 			getSymbol = " X ";
-			tempNum = Double.parseDouble(calcText.getText()) / 100;
+			tempNum = Double.parseDouble(calcText.getText()) / 100; // Formats the result after multiplication
 			calcText.setText(numberFormating(tempNum));
 			break;
 		case 2:
 			getSymbol = "/";
-			tempNum = Double.parseDouble(calcText.getText()) * 100;
+			tempNum = Double.parseDouble(calcText.getText()) * 100; // Formats the result after division
 			calcText.setText(numberFormating(tempNum));
 			break;
 		case 3:
@@ -377,6 +408,7 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 			break;
 		}
 		
+		// Puts the operation formated in "holdText"
 		holdText.setText(numberFormating(firstN) + getSymbol + numberFormating(holdN) + "%");
 		
 	}
@@ -384,14 +416,18 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 	private void runopt() {
 		
 		if(repeat) {
+			// Dont loses the working value when user repeats the operation
 			secN = holdN;
 			firstN = Double.parseDouble(calcText.getText());
 		}
 		else {
+			// Holds the working value
 			holdN = secN;
 		}
 		
 		switch(opt) {
+		// Do the basic operations
+		// 1-Multiplication, 2-Division, 3-Sum, 4-Subtraction
 		case 1:
 			calcText.setText(numberFormating(firstN * secN));
 			holdText.setText(numberFormating(firstN) + " X " + numberFormating(secN));
@@ -421,19 +457,22 @@ public class SimpleCalculator extends JFrame implements ActionListener {
 
 	private String numberFormating(double num) {
 		
+		// This function removes 0s in the decimal places if there is only 0s
+		
 		String tempNum[] = {
 				"" + num
 		};
 		
-		tempNum = tempNum[0].split("\\.");
+		tempNum = tempNum[0].split("\\."); // Regex to split the number in the decimal
 		
-		if(tempNum[1].matches("[0]+"))
+		if(tempNum[1].matches("[0]+")) // Regex to gets the zeroes
 			return tempNum[0];
 		else
 			return tempNum[0] + "." + tempNum[1];
 	}
 
 	public static void main(String[] args) {
+		
 		new SimpleCalculator();
 
 	}
